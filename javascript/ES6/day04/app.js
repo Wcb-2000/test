087eb06a1182a0c9c8c7ecd3425eb6e48114ac3f
Wcb-2000 <846583697@qq.com>
2021-05-18 23:43:24 +0800

    标签引入模块

---
+++ javascript/ES6/day04/app.js
@@ -0,0 +1,17 @@
+/*
+ * Author: 吴楚标
+ * Date: 2021-05-18 00:03:21
+ * LastEditors: 吴楚标
+ * LastEditTime: 2021-05-18 00:09:26
+ * Description: 
+*/
+//入口文件
+
+//模块引入
+import * as m1 from "./m1.js";
+import * as m2 from "./m2.js";
+import * as m3 from "./m3.js";
+
+console.log(m1);
+console.log(m2);
+console.log(m3);
