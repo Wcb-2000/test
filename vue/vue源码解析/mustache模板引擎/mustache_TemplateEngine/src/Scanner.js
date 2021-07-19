321a74f18eaab38eed0a177f39066c984e323ef3
Wcb-2000 <846583697@qq.com>
2021-07-19 22:08:48 +0800

    扫描文本识别{{}}

--- vue/vue源码解析/mustache模板引擎/mustache_TemplateEngine/src/Scanner.js
+++ vue/vue源码解析/mustache模板引擎/mustache_TemplateEngine/src/Scanner.js
@@ -2,7 +2,7 @@
  * Author: 吴楚标
  * Date: 2021-07-18 16:40:17
  * LastEditors: 吴楚标
- * LastEditTime: 2021-07-18 18:06:58
+ * LastEditTime: 2021-07-19 15:20:18
  * Description: 
 */
 /**
@@ -30,9 +30,9 @@ export default class Scanner {
   }
 
   // 让指针进行扫描，直到遇见指定内容结束，并且能够返回结束之前路过的文字
-  scanUtil(stopTag){
+  scanUntil(stopTag){
     // 记录一下执行本方法的时候pos的值
-    const post_backup = this.pos;
+    const pos_backup = this.pos;
     // 当尾巴的开头不是stopTag的时候，说明还没有扫描到stopTag
     // 写&&很有必要,因为防止找不到,那么到最后也要停下来,防止死循环
     while(!this.eos() && this.tail.indexOf(stopTag) != 0  ){
@@ -40,11 +40,11 @@ export default class Scanner {
       // 改变尾巴为从当前指针这个字符开始，到最后的全部字符
       this.tail = this.templateStr.substring(this.pos);
     }
-    return this.templateStr.substring(post_backup, this.pos);
+    return this.templateStr.substring(pos_backup, this.pos);
   }
  
   // 判断指针是否已到结尾,返回布尔值, end of string
   eos(){
-    return this.pos < this.templateStr.length;
+    return this.pos >= this.templateStr.length;
   }
 }
